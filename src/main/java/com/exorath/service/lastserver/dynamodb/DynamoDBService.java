6d02852fc24c2b936d43870d2c76a7cc4faa9853
Yannick <yannickjeras@hotmail.com>
2016-11-05 16:13:41 +0000

    First pass dynamodb implementation

---
+++ src/main/java/com/exorath/service/lastserver/dynamodb/DynamoDBService.java
@@ -0,0 +1,116 @@
+/*
+ * Copyright 2016 Exorath
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.exorath.service.lastserver.dynamodb;
+
+import com.amazonaws.services.dynamodbv2.document.AttributeUpdate;
+import com.amazonaws.services.dynamodbv2.document.Item;
+import com.amazonaws.services.dynamodbv2.document.Table;
+import com.amazonaws.services.dynamodbv2.document.UpdateItemOutcome;
+import com.amazonaws.services.dynamodbv2.document.spec.GetItemSpec;
+import com.amazonaws.services.dynamodbv2.document.spec.UpdateItemSpec;
+import com.amazonaws.services.dynamodbv2.model.AttributeDefinition;
+import com.amazonaws.services.dynamodbv2.model.ConditionalCheckFailedException;
+import com.amazonaws.services.dynamodbv2.model.CreateTableRequest;
+import com.amazonaws.services.dynamodbv2.model.KeySchemaElement;
+import com.amazonaws.services.dynamodbv2.model.KeyType;
+import com.amazonaws.services.dynamodbv2.model.ProvisionedThroughput;
+import com.amazonaws.services.dynamodbv2.model.ResourceInUseException;
+import com.amazonaws.services.dynamodbv2.model.ScalarAttributeType;
+import com.exorath.service.commons.dynamoDBProvider.DynamoDBProvider;
+import com.exorath.service.lastserver.Service;
+import com.exorath.service.lastserver.res.LastServer;
+import com.exorath.service.lastserver.res.Result;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.UUID;
+
+public class DynamoDBService implements Service {
+	private static final String TABLE_NAME = "LastServer";
+	private static final String PRIM_KEY = "playerId";
+	private static final String GAMEID_ATTR = "gameId";
+	private static final String MAPID_ATTR = "mapId";
+	private static final String FLAVORID_ATTR = "flavorId";
+	private static final Logger logger = LoggerFactory.getLogger(DynamoDBService.class);
+
+	private Table table;
+
+	public DynamoDBService(DynamoDBProvider provider) {
+		try {
+			table = provider.getDB().createTable(new CreateTableRequest()
+					.withTableName(TABLE_NAME)
+					.withKeySchema(new KeySchemaElement(PRIM_KEY, KeyType.HASH))
+					.withAttributeDefinitions(new AttributeDefinition(PRIM_KEY, ScalarAttributeType.S))
+					.withProvisionedThroughput(new ProvisionedThroughput(1L, 1L))
+			);
+			logger.info("Created DynamoDB table " + TABLE_NAME + " with 1r/1w provisioning. Waiting for it to activate.");
+		} catch (ResourceInUseException ex) {
+			table = provider.getDB().getTable(TABLE_NAME);
+			logger.info("DynamoDB table " + TABLE_NAME + " already existed. Waiting for it to activate.");
+		}
+
+		try {
+			table.waitForActive();
+		} catch (InterruptedException ex) {
+			logger.error("DynamoDB table " + TABLE_NAME + " could not activate!\n" + ex.getMessage());
+			System.exit(1);
+		}
+		logger.info("DynamoDB table " + TABLE_NAME + " active.");
+	}
+
+
+	@Override
+	public LastServer getLastServer(UUID playerId) {
+		GetItemSpec spec = new GetItemSpec().withPrimaryKey(PRIM_KEY, playerId.toString());
+		Item item = table.getItem(spec);
+		logger.info("Retrieved the following last server data for player " + playerId + ": " + item);
+		if (item == null || !item.hasAttribute(GAMEID_ATTR)) {
+			return new LastServer(null, null, null);
+		} else {
+			String gameId = null;
+			String mapId = null;
+			String flavorId = null;
+			if (item.hasAttribute(GAMEID_ATTR)) {
+				gameId = item.getString(GAMEID_ATTR);
+			}
+			if (item.hasAttribute(MAPID_ATTR)) {
+				mapId = item.getString(MAPID_ATTR);
+			}
+			if (item.hasAttribute(FLAVORID_ATTR)) {
+				flavorId = item.getString(FLAVORID_ATTR);
+			}
+			return new LastServer(gameId, mapId, flavorId);
+		}
+	}
+
+	@Override
+	public Result setLastServer(UUID playerId, String gameId, String mapId, String flavorId) {
+		UpdateItemSpec spec = new UpdateItemSpec()
+				.withPrimaryKey(PRIM_KEY, playerId.toString())
+				.withAttributeUpdate(new AttributeUpdate(GAMEID_ATTR).put(gameId))
+				.withAttributeUpdate(new AttributeUpdate(MAPID_ATTR).put(mapId))
+				.withAttributeUpdate(new AttributeUpdate(FLAVORID_ATTR).put(flavorId));
+		try {
+			UpdateItemOutcome outcome = table.updateItem(spec);
+			logger.info("Successfully set last server data for player " + playerId + ": gameId(" + gameId + ") mapId(" + mapId + ") flavorId(" + flavorId + ")");
+			return new Result(true);
+		} catch (ConditionalCheckFailedException ex) {
+			logger.warn("Failed to set last server data for player" + playerId + ": gameId(" + gameId + ") mapId(" + mapId + ") flavorId(" + flavorId + ")\n:" + ex.getMessage());
+			return new Result(false);
+		}
+	}
+}
