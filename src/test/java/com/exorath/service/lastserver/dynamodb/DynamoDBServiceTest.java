0dc0dc55cacfc1c164477d919bf84fd0d90c9d1d
Yannick <yannickjeras@hotmail.com>
2016-11-08 18:43:04 +0000

    Test dynamodb functionality

--- src/test/java/com/exorath/service/lastserver/dynamodb/DynamoDBServiceTest.java
+++ src/test/java/com/exorath/service/lastserver/dynamodb/DynamoDBServiceTest.java
@@ -18,23 +18,42 @@ package com.exorath.service.lastserver.dynamodb;
 
 import com.amazonaws.services.dynamodbv2.document.DynamoDB;
 import com.exorath.service.lastserver.Service;
+import com.exorath.service.lastserver.res.GetResult;
+import com.exorath.service.lastserver.res.PutResult;
+import org.junit.Assert;
 import org.junit.ClassRule;
 import org.junit.Test;
 
 import java.util.UUID;
 
 public class DynamoDBServiceTest {
+	private static final String NULL_STRING = "NULL";
+	private static final String GAME_ID_2 = "gameId2";
+	private static final String MAP_ID_2 = "mapId2";
+	private static final String FLAVOR_ID_2 = "flavorId2";
 
 	@ClassRule
 	public static final LocalDynamoDBCreationRule client = new LocalDynamoDBCreationRule();
 
 	@Test
-	public void testGetTreasuresEqualsSetTreasures() {
+	public void testGetLastServerEqualsSetLastServer() {
 		DynamoDB db = new DynamoDB(client.getAmazonDynamoDB());
 		Service srv = new DynamoDBService(() -> db);
 
 		UUID playerId = UUID.randomUUID();
 		UUID playerId2 = UUID.randomUUID();
+		PutResult putResult = srv.setLastServer(playerId, null, NULL_STRING, NULL_STRING);
+		Assert.assertEquals(false, putResult.isSuccess());
 
+		PutResult putResult2 = srv.setLastServer(playerId2, GAME_ID_2, MAP_ID_2, FLAVOR_ID_2);
+		Assert.assertEquals(true, putResult2.isSuccess());
+
+		GetResult getResult = srv.getLastServer(playerId);
+		Assert.assertEquals(null, getResult.getGameId());
+
+		GetResult getResult2 = srv.getLastServer(playerId2);
+		Assert.assertEquals(GAME_ID_2, getResult2.getGameId());
+		Assert.assertEquals(MAP_ID_2, getResult2.getMapId());
+		Assert.assertEquals(FLAVOR_ID_2, getResult2.getFlavorId());
 	}
 }
